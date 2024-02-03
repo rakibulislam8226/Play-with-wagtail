1e33e1118ad60d5ca2803d8a348116e5ed6fdb13
rakibulislam8226 <rakibulislam8226@gmail.com>
2024-02-04 01:18:04 +0600

    search methodology added

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,8 +1,9 @@
 # urls.py
 from django.urls import path
 
-from .views import blog_list
+from .views import blog_list, search_view
 
 urlpatterns = [
     path("", blog_list, name="blog_list"),
+    path("search/", search_view, name="search_view"),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
+from wagtail.models import Page
+from wagtail.search.backends import get_search_backend
+
 from .models import BlogPage
 
 
 def blog_list(request):
     blog_posts = BlogPage.objects.live().public()
     return render(request, "blog/blog_list.html", {"blog_posts": blog_posts})
+
+
+def search_view(request):
+    query = request.GET.get("q", "")
+
+    # Use Wagtail's search backend to get search results
+    search_backend = get_search_backend()
+    search_results = search_backend.search(query, Page)
+
+    context = {
+        "query": query,
+        "search_results": search_results,
+    }
+
+    return render(request, "search/search_results.html", context)
